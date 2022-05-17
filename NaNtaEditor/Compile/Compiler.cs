cf6242db4baeedcd05cd6d283bc0eb2580f76c21
Robertzzel <robertzel_2001@yahoo.com>
2022-05-17 17:27:30 +0300

    Implemented build message in textBox and app run in console

--- NaNtaEditor/Compile/Compiler.cs
+++ NaNtaEditor/Compile/Compiler.cs
@@ -15,113 +15,29 @@ namespace Editor.Compile
         private const string OUTPUT_FILE_NAME = "main";
         private const string COMPILE_ARGUMENTS = $"-o {OUTPUT_FILE_NAME}";
 
-        private TextBox? _textBox;
-        private string _undeDeletableText = "";
-        private System.IO.StreamWriter? _standardInput;
-
-        public void RunFileInTerminal(string fullPath)
+        public void RunFileInTerminal(string fullPath, TextBox textBox)
         {
-            string comandaCompilare = $"{COMPILER_NAME} {fullPath} {COMPILE_ARGUMENTS}";
             string comandaRulare = $"{OUTPUT_FILE_NAME}.exe";
             string comandaAsteptareTastatura = "echo. && echo. && echo Process returned status code %errorlevel% && set /p stat =\"Press any key to exit...\"";
+
+            string? errors = BuildFile(fullPath);
+            if (errors != null)
+            {
+                textBox.Text = errors;
+                return;
+            }
+            textBox.Text = "Build succeeded";
 
             var process = new Process
             {
                 StartInfo = new ProcessStartInfo
                 {
                     FileName = "CMD.exe",
-                    Arguments = $"/C {comandaCompilare} && {comandaRulare} & {comandaAsteptareTastatura}",
+                    Arguments = $"/C {comandaRulare} & {comandaAsteptareTastatura}",
                 }
             };
 
             process.Start();
-        }
-
-        public void RunFile(string fullPath, TextBox outputTextBox)
-        {
-            this._textBox = outputTextBox;
-
-            string? compilation_errors = BuildFile(fullPath);
-            if (compilation_errors != null)
-            {
-                this._textBox.Text += $"{compilation_errors}\r\n";
-                return;
-            }
-
-            var process = new Process
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = OUTPUT_FILE_NAME,
-                    CreateNoWindow = true,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    RedirectStandardInput = true
-                }
-            };
-
-            process.Start();
-            this._standardInput = process.StandardInput;
-            var standartOutput = process.StandardOutput;
-            var standardError = process.StandardError;
-
-            var taskStdOut = Task.Run(() =>
-            {
-                while (!standartOutput.EndOfStream)
-                {
-                    var character = ((char)standartOutput.Read()).ToString();
-                    this._undeDeletableText = this._textBox.Text+character;
-
-                    this._textBox.BeginInvoke((MethodInvoker)delegate ()
-                    {
-                        this._textBox.AppendText(character);
-                    });
-                }
-            });
-
-            var taskStdErr = Task.Run(() =>
-            {
-                while (!standardError.EndOfStream)
-                {
-                    var character = ((char)standardError.Read()).ToString();
-                    this._undeDeletableText = this._textBox.Text+character;
-
-
-                    this._textBox.BeginInvoke((MethodInvoker)delegate ()
-                    {
-                        this._textBox.AppendText(character);
-                    });
-                }
-            });
-
-
-            this._textBox.TextChanged += new EventHandler(textListener);
-            process.Exited += new EventHandler((object? sender, EventArgs e) =>
-            {
-                string exitMessage = "\r\nProcess finished";
-                this._undeDeletableText = this._textBox.Text + exitMessage;
-                this._textBox.Text += exitMessage;
-            });
-
-        }
-
-        public void textListener(object? sender, EventArgs e)
-        {
-            if (this._textBox!.Text == this._undeDeletableText)
-                return;
-
-            var currentText = this._textBox!.Text;
-            var newText = currentText.Substring(this._undeDeletableText.Length, currentText.Length - this._undeDeletableText.Length);
-
-            if (!newText.Contains("\r\n"))
-                return;
-
-            foreach(string command in newText.Split("\r\n"))
-            {
-                this._standardInput!.WriteLine(command);
-            }
-            this._undeDeletableText += newText;
-            
         }
 
         public static string? BuildFile(string fullPath)
@@ -133,6 +49,7 @@ namespace Editor.Compile
                     FileName = COMPILER_NAME,
                     Arguments = $"{fullPath} {COMPILE_ARGUMENTS}",
                     RedirectStandardError = true,
+                    CreateNoWindow = true
                 }
             };
             process.Start();
